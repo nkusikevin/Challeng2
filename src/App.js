d65c3559b6dc44fb418b9dedcb08520a7c17d562
“nkusikevin” <nkusikvnhart@gmail.com>
2021-07-18 22:08:04 +0200

    fix errors

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
 	const albumId = 1;
 	const [id, setId] = useState("");
 	const [pics, setPics] = useState(null);
-	const [loading, setLoading] = useState(false);
+	const [loading, setLoading] = useState(true);
 	const [notFound, setNotFound] = useState(false);
 	const getId = (e) => {
 		setPics(null);
 		setId(e.target.value);
 		setNotFound(false);
 	};
-	const handleClick = async () => {
+	const handleClick = async() => {
 		if (id) {
 			const { data } = await axios.get(
 				`https://jsonplaceholder.typicode.com/albums/${id}/photos`
@@ -42,12 +42,15 @@ function App() {
 			</nav>
 			<div className='main'>
 				<ul className='cards'>
-					{notFound && <NotFound/>}
-					{loading ? <Loader /> : <Card />}
-					{pics.map((pic)=>(
-						<Card title={pic.title} thumbnail={pic.thumbnail}/>
-					))}
-					</ul>
+					{notFound && <NotFound />}
+					{loading ? (
+						<Loader />
+					) : (
+						pics.map((pic) => (
+							<Card title={pic.title} thumbnail={pic.thumbnail} />
+						))
+					)}
+				</ul>
 			</div>
 			<Footer />
 		</>
